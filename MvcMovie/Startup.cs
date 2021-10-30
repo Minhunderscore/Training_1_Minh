a1561341220f6836711d1c7223495ee68b881819
Minhunderscore <86943747+Minhunderscore@users.noreply.github.com>
2021-10-30 16:18:06 +0700

    Part 5: Done!

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -1,3 +1,5 @@
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,24 +10,26 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MvcMovie.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace MvcMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    {
+        Environment = env;
+        Configuration = configuration;
+    }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
